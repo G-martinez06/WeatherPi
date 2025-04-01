432668dd5302b068474877192bc45c180dc87556
G-martinez06 <146362110+G-martinez06@users.noreply.github.com>
2025-04-01 14:09:01 -0500

    Add files via upload

--- tm1637.py
+++ tm1637.py
@@ -1,226 +1,242 @@
-"""
-MicroPython TM1637 quad 7-segment LED display driver
-https://github.com/mcauser/micropython-tm1637
+# TM1637.py
+# Version 1.00
 
-MIT License
-Copyright (c) 2016-2023 Mike Causer
+import RPi.GPIO as GPIO
+from time import sleep
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+PATTERN = b'\0\x86\x22\0\0\0\0\x02\0\0\0\0\x04\x40\x80\x52\x3f\x06\x5b\x4f\x66\x6d\x7d\x07\x7f\x6f\0\0\0\x48\0\0\x5d\x77\x7c\x39\x5e\x79\x71\x3d\x76\x30\x0e\x70\x38\x55\x54\x3f\x73\x67\x50\x2d\x78\x3e\x36\x6a\x49\x6e\x1b\x39\x64\x0f\x23\x08\x20\x77\x7c\x58\x5e\x79\x71\x3d\x74\x10\x0c\x70\x30\x55\x54\x5c\x73\x67\x50\x2d\x78\x1c\x36\x6a\x49\x6e\x1b\0\x30\0\x41'
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+class FourDigit:
+    '''
+    Abstraction of the 4 digit 7-segment display based on the TM1637 display driver.
+    7-bit ASCII characters are mapped as close as possible to the 7 display segments. If a 
+    character cannot be mapped, the digit is cleared.
+    '''
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
+    myData = [0,0,0,0]
+    
+    @staticmethod
+    def getDisplayableChars():
+        '''
+        Returns a string with all displayable characters taken from PATTERN dictionary.
+        @return: The character set that can be displayed
+        '''
+        s = "<SPACE>"
+        k = 33
+        while k < 127:
+            ch = chr(k)
+            if TM1637.PATTERN[ch] != 0:
+                s = s + ch
+            k += 1
+        return  s
 
-__version__ = '1.3.0'
+    @staticmethod
+    def toHex(intValue):
+        '''
+        Returns a string with hex digits from given number (>0, any size).
+        @param number: the number to convert (must be positive)
+        @return: string of hex digits (uppercase), e.g. 0xFE
+        '''
+        return '%02x' % intValue
 
-from micropython import const
-from machine import Pin
-from time import sleep_us, sleep_ms
+    @staticmethod
+    def toBytes(intValue):
+        '''
+        Returns a list of four byte values [byte#24-#31, byte#16-#23, byte#8-#15, byte#0-#7] of given integer.
+        @param number: an integer
+        @return: list with integers of 4 bytes [MSB,..., LSB]
+        '''
+        byte0 = intValue & 0xff
+        byte1 = (intValue >> 8) & 0xff
+        byte2 = (intValue >> 16) & 0xff
+        byte3 = (intValue >> 24) & 0xff
+        return [byte3, byte2, byte1, byte0]
 
-TM1637_CMD1 = const(64)  # 0x40 data command
-TM1637_CMD2 = const(192) # 0xC0 address command
-TM1637_CMD3 = const(128) # 0x80 display control command
-TM1637_DSP_ON = const(8) # 0x08 display on
-TM1637_DELAY = const(10) # 10us delay between clk/dio pulses
-TM1637_MSB = const(128)  # msb is the decimal point or the colon depending on your display
-
-# 0-9, a-z, blank, dash, star
-_SEGMENTS = bytearray(b'\x3F\x06\x5B\x4F\x66\x6D\x7D\x07\x7F\x6F\x77\x7C\x39\x5E\x79\x71\x3D\x76\x06\x1E\x76\x38\x55\x54\x3F\x73\x67\x50\x6D\x78\x3E\x1C\x2A\x76\x6E\x5B\x00\x40\x63')
-
-class TM1637(object):
-    """Library for quad 7-segment LED modules based on the TM1637 LED driver."""
-    def __init__(self, clk, dio, brightness=7):
+    @staticmethod
+    def toInt(hexValue):
+        '''
+        Returns an integer from given hex string
+        @param number: a string with the number to convert, e.g. "FE" or "fe" or "0xFE" or "0XFE"
+        @return: integer number
+        '''
+        return int(hexValue, 16)
+    
+    # ------------------- Constructor ---------------------------------        
+    def __init__(self, dio = 38, clk = 40, lum = 4):
+        '''
+        Creates a display instance that uses the two given GPIO pins for data (dio, default: 38) and 
+        clock (clk, default: 40).
+        It is set to the given luminosiy (0..9, default: 4)
+        '''
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
         self.clk = clk
         self.dio = dio
+        self.lum = lum
+        self.colon = False
+        self.startPos = 0
+        self.text = None
+        GPIO.setup(clk, GPIO.OUT)
+        GPIO.setup(dio, GPIO.OUT)
+        GPIO.output(clk, 0)
+        GPIO.output(dio, 0)
 
-        if not 0 <= brightness <= 7:
-            raise ValueError("Brightness out of range")
-        self._brightness = brightness
+    # ------------------- Public methods ---------------------------------        
+    def erase(self):
+        '''
+        Clears the display (all digits are turned off).
+        '''
+        self.colon = False
+        self.show("    ")
 
-        self.clk.init(Pin.OUT, value=0)
-        self.dio.init(Pin.OUT, value=0)
-        sleep_us(TM1637_DELAY)
+    def show(self, text, pos = 0):
+        '''
+        Displays 4 characters of the given text. The text is considered to be prefixed and postfixed by spaces
+        and the 4 character window is selected by the text pointer pos that determines the character displayed at the
+        leftmost digit, e.g. (_: empty):
+        showText("AbCdEF") -> AbCd
+        showText("AbCdEF", 1) -> bCdE
+        showText("AbCdEF", -1) ->_AbC
+        showText("AbCdEF", 4) -> EF__
+        @param text: the text to display (string or integer)
+        @param pos: the start value of the text pointer (character index positioned a leftmost digit)
+        '''
+        self.startPos = pos
+        self.pos = pos
+        self.text = str(text)  # digits to chars
+        if len(self.text) < 4:
+            self.text = "%-4s" % self.text
+        self._cropText()
+        
+    def scroll(self, text):
+        '''
+        Starts scrolling the text to the left and blocks until all characters have passed by.
+        '''
+        self.show(text)
+        sleep(1.5)
+        while self.toLeft() > 0:
+            sleep(1)
+        sleep(1)    
+            
+    def toRight(self):
+        '''
+        Scrolls the current text one step to the left by decreasing the text pointer.
+        @return: the number of characters hidden, but remaining to be displayed at the left (>=0); -1, if error
+        '''
+        if self.text == None:
+            return -1
+        self.pos -= 1    
+        self._cropText()
+        return max(0, 4 + self.pos)
 
-        self._write_data_cmd()
-        self._write_dsp_ctrl()
+    def toLeft(self):
+        '''
+        Scrolls the current text one step to the left by increasing the text pointer.
+        @return: the number of characters hidden, but remaining to be displayed at the right (>=0); -1, if error
+        '''
+        if self.text == None:
+            return -1
+        self.pos += 1    
+        self._cropText()
+        nb = len(self.text) - self.pos
+        return max(0, nb)
+
+    def toStart(self):
+        '''
+        Shows the text at the start position by setting the text pointer to its start value.
+        '''
+        if self.text == None:
+            return -1
+        self.pos = self.startPos    
+        self._cropText()
+       
+    def setLuminosity(self, lum):
+        '''
+        Sets the brightness of the display.
+        @param luminosity the brightness (0..9, 0: invisible)
+        '''
+        self.lum = lum
+
+    def setColon(self, enable):        
+        '''
+        Enables/disables the colon in the middle of the screen.
+        @param enable if True, the colon is shown in all subsequent text operations
+        '''
+        self.colon = enable
+        
+    # ------------------- end of public methods ---------------------------------        
+
+    def _cropText(self):
+        n = len(self.text)
+        data = [' '] * (n + 8)
+        for i in range(n):
+            data[i + 4] = self.text[i]
+        start = max(0, self.pos + 4)
+        start = min(start, len(data) - 4)
+        end = min(start + n, len(data))
+        data = self._toSegment(data[start:end])
+        self._prepare(0x40)    
+        self._writeByte(0xC0)
+        for i in range(4):
+            self._writeByte(data[i])
+        self._commit()
+        
+    def _writeByte(self, data):
+        for i in range(8):
+            GPIO.output(self.clk, 0)
+            sleep(0.0001)
+            if data & 0x01:
+                GPIO.output(self.dio, 1)
+            else:
+                GPIO.output(self.dio, 0)
+            sleep(0.0001)
+            data = data >> 1
+            GPIO.output(self.clk, 1)
+            sleep(0.0001)
+
+        GPIO.output(self.clk, 0)
+        GPIO.output(self.dio, 1)
+        GPIO.output(self.clk, 1)
+        # wait for ACK, no need to set pin as input
+        while GPIO.input(self.dio) == 1:
+            sleep(0.001)
+        sleep(0.001)
+    
+    def _toSegment(self, text):
+        data = []
+        msb = 0
+        if self.colon:
+            msb = 0x80
+        for c in text:
+            if ord(c) < 32 or ord(c) > 127:
+                c = " "
+            data.append(ord(PATTERN[ord(c) - 32]) + msb)
+        return data
 
     def _start(self):
-        self.dio(0)
-        sleep_us(TM1637_DELAY)
-        self.clk(0)
-        sleep_us(TM1637_DELAY)
-
+        GPIO.output(self.clk, 1)
+        GPIO.output(self.dio, 1)
+        sleep(0.0001)
+        GPIO.output(self.dio, 0) 
+        GPIO.output(self.clk, 0) 
+        sleep(0.0001)
+    
     def _stop(self):
-        self.dio(0)
-        sleep_us(TM1637_DELAY)
-        self.clk(1)
-        sleep_us(TM1637_DELAY)
-        self.dio(1)
-
-    def _write_data_cmd(self):
-        # automatic address increment, normal mode
+        GPIO.output(self.clk, 0) 
+        GPIO.output(self.dio, 0) 
+        sleep(0.0001)
+        GPIO.output(self.clk, 1)
+        GPIO.output(self.dio, 1)
+        sleep(0.0001)
+        
+    def _prepare(self, addr):        
         self._start()
-        self._write_byte(TM1637_CMD1)
+        self._writeByte(addr)
         self._stop()
-
-    def _write_dsp_ctrl(self):
-        # display on, set brightness
-        self._start()
-        self._write_byte(TM1637_CMD3 | TM1637_DSP_ON | self._brightness)
-        self._stop()
-
-    def _write_byte(self, b):
-        for i in range(8):
-            self.dio((b >> i) & 1)
-            sleep_us(TM1637_DELAY)
-            self.clk(1)
-            sleep_us(TM1637_DELAY)
-            self.clk(0)
-            sleep_us(TM1637_DELAY)
-        self.clk(0)
-        sleep_us(TM1637_DELAY)
-        self.clk(1)
-        sleep_us(TM1637_DELAY)
-        self.clk(0)
-        sleep_us(TM1637_DELAY)
-
-    def brightness(self, val=None):
-        """Set the display brightness 0-7."""
-        # brightness 0 = 1/16th pulse width
-        # brightness 7 = 14/16th pulse width
-        if val is None:
-            return self._brightness
-        if not 0 <= val <= 7:
-            raise ValueError("Brightness out of range")
-
-        self._brightness = val
-        self._write_data_cmd()
-        self._write_dsp_ctrl()
-
-    def write(self, segments, pos=0):
-        """Display up to 6 segments moving right from a given position.
-        The MSB in the 2nd segment controls the colon between the 2nd
-        and 3rd segments."""
-        if not 0 <= pos <= 5:
-            raise ValueError("Position out of range")
-        self._write_data_cmd()
         self._start()
 
-        self._write_byte(TM1637_CMD2 | pos)
-        for seg in segments:
-            self._write_byte(seg)
+    def _commit(self):
         self._stop()
-        self._write_dsp_ctrl()
-
-    def encode_digit(self, digit):
-        """Convert a character 0-9, a-f to a segment."""
-        return _SEGMENTS[digit & 0x0f]
-
-    def encode_string(self, string):
-        """Convert an up to 4 character length string containing 0-9, a-z,
-        space, dash, star to an array of segments, matching the length of the
-        source string."""
-        segments = bytearray(len(string))
-        for i in range(len(string)):
-            segments[i] = self.encode_char(string[i])
-        return segments
-
-    def encode_char(self, char):
-        """Convert a character 0-9, a-z, space, dash or star to a segment."""
-        o = ord(char)
-        if o == 32:
-            return _SEGMENTS[36] # space
-        if o == 42:
-            return _SEGMENTS[38] # star/degrees
-        if o == 45:
-            return _SEGMENTS[37] # dash
-        if o >= 65 and o <= 90:
-            return _SEGMENTS[o-55] # uppercase A-Z
-        if o >= 97 and o <= 122:
-            return _SEGMENTS[o-87] # lowercase a-z
-        if o >= 48 and o <= 57:
-            return _SEGMENTS[o-48] # 0-9
-        raise ValueError("Character out of range: {:d} '{:s}'".format(o, chr(o)))
-
-    def hex(self, val):
-        """Display a hex value 0x0000 through 0xffff, right aligned."""
-        string = '{:04x}'.format(val & 0xffff)
-        self.write(self.encode_string(string))
-
-    def number(self, num):
-        """Display a numeric value -999 through 9999, right aligned."""
-        # limit to range -999 to 9999
-        num = max(-999, min(num, 9999))
-        string = '{0: >4d}'.format(num)
-        self.write(self.encode_string(string))
-
-    def numbers(self, num1, num2, colon=True):
-        """Display two numeric values -9 through 99, with leading zeros
-        and separated by a colon."""
-        num1 = max(-9, min(num1, 99))
-        num2 = max(-9, min(num2, 99))
-        segments = self.encode_string('{0:0>2d}{1:0>2d}'.format(num1, num2))
-        if colon:
-            segments[1] |= 0x80 # colon on
-        self.write(segments)
-
-    def temperature(self, num):
-        if num < -9:
-            self.show('lo') # low
-        elif num > 99:
-            self.show('hi') # high
-        else:
-            string = '{0: >2d}'.format(num)
-            self.write(self.encode_string(string))
-        self.write([_SEGMENTS[38], _SEGMENTS[12]], 2) # degrees C
-
-    def show(self, string, colon=False):
-        segments = self.encode_string(string)
-        if len(segments) > 1 and colon:
-            segments[1] |= 128
-        self.write(segments[:4])
-
-    def scroll(self, string, delay=250):
-        segments = string if isinstance(string, list) else self.encode_string(string)
-        data = [0] * 8
-        data[4:0] = list(segments)
-        for i in range(len(segments) + 5):
-            self.write(data[0+i:4+i])
-            sleep_ms(delay)
-
-
-class TM1637Decimal(TM1637):
-    """Library for quad 7-segment LED modules based on the TM1637 LED driver.
-
-    This class is meant to be used with decimal display modules (modules
-    that have a decimal point after each 7-segment LED).
-    """
-
-    def encode_string(self, string):
-        """Convert a string to LED segments.
-
-        Convert an up to 4 character length string containing 0-9, a-z,
-        space, dash, star and '.' to an array of segments, matching the length of
-        the source string."""
-        segments = bytearray(len(string.replace('.','')))
-        j = 0
-        for i in range(len(string)):
-            if string[i] == '.' and j > 0:
-                segments[j-1] |= TM1637_MSB
-                continue
-            segments[j] = self.encode_char(string[i])
-            j += 1
-        return segments
+        self._start()
+        self._writeByte(0x88 + self.lum)
+        self._stop() 
